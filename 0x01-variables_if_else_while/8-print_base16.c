75b78b785800dc80c60abb3c2698954b1f7a858d
ahmed ibrahim <ahmadibrahim1234567890@gmail.com>
2023-08-11 16:01:35 -0700

    Update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* This program will assign a random number to the variable */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c <= 'f';	c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
