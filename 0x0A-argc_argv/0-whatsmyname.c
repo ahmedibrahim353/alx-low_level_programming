c1fb10acf2686ba4020bc1afec37f190250aff77
ahmed ibrahim <ahmadibrahim1234567890@gmail.com>
2023-09-01 13:01:35 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - check the code
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+return (0);
+
+}
