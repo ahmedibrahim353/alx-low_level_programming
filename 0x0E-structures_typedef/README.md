d7bc3312a7e3b071c7f50b10ee8d7e8c544f6f89
ahmed ibrahim <ahmadibrahim1234567890@gmail.com>
2023-09-12 15:00:41 -0700

    dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+srtuctures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * struct dog -  Short description
+ *
+ * @name: The dog name
+ * @age: Thr dog age
+ * @owner: The dog owner
+ *
+ * Description: Longer description
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
