b3f7b3ae2995a91276e555d32869a82d05b05fb8
ahmed ibrahim <ahmadibrahim1234567890@gmail.com>
2023-09-15 14:25:46 -0700

    str

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,8 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (str == NULL)
 				printf("(nil)");
 
-
-		printf("%s", str);
+			else printf("%s", str);
 
 		if (i != n - 1 && separator != NULL)
 
