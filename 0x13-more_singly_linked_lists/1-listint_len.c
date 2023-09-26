d41a056ec33ee0b23244f5ed59dd9246d521fdbd
ahmedibrahim353 <ahmadibrahim1234567890@gmail.com.com>
2023-09-26 09:15:40 -0700

    node_no

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints number of the lists.
+ *
+ * @h: pointer to first node
+ *
+ * Return: the number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t node_no = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+
+		h = h->next;
+
+		node_no++;
+	}
+
+	return (node_count);
+}
